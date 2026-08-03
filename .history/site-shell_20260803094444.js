1a258400dbd41bafd0a5f7a73d7f38cf3af7ada1
argyibug <argyibug@outlook.com>
2026-08-03 09:45:47 +0800

    update google

--- .history/site-shell_20260803094444.js
+++ .history/site-shell_20260803094444.js
@@ -7,7 +7,7 @@
   const NAV_ITEMS = [
     { key: 'home', href: 'index.html', label: 'Home' },
     { key: 'publications', href: 'publications.html', label: 'Publications' },
-    { key: 'projects', href: 'projects.html', label: 'project&blog' },
+    { key: 'projects', href: 'projects.html', label: 'Project & Blog' },
     { key: 'blogs', href: 'blogs.html', label: 'Academic Activity' },
     { key: 'cv', href: 'files/ZCK_resume.pdf', label: 'CV' }
   ];
@@ -24,7 +24,7 @@
       icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>'
     },
     {
-      href: 'https://scholar.google.com/citations?user=aALMUAwAAAAJ&hl=zh-TW&oi=ao',
+      href: 'https://scholar.google.com/citations?user=C50jgToAAAAJ&hl=zh-TW',
       label: 'Google Scholar',
       icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"/></svg>'
     }
